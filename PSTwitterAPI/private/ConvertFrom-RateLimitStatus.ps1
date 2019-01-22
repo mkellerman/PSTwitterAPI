9fc044c5a320493a5133905a33c3c67a94fa389a
Marc R Kellerman <9411143+mkellerman@users.noreply.github.com>
2019-01-22 11:41:30 -0800

    Fix ForEach syntax

--- PSTwitterAPI/private/ConvertFrom-RateLimitStatus.ps1
+++ PSTwitterAPI/private/ConvertFrom-RateLimitStatus.ps1
@@ -2,14 +2,23 @@ function ConvertFrom-RateLimitStatus ($RateLimitStatus) {
 
     $Eposh = Get-Eposh
 
-    $RateLimitStatus.resources.PSObject.Properties | ForEach {
-        $_.value | ForEach {
-            $_.PSObject.Properties | ForEach {
-                $_ | Select-Object @{ n='accesstoken'; e={ $RateLimitStatus.rate_limit_context.access_token }}, @{ n='resource'; e={ $_.name }}, @{ n='limit'; e={ $_.value.limit }}, @{ n='remaining'; e={ $_.value.remaining }}, @{ n='reset'; e={ $_.value.reset }}
+    $RateLimitStatus.resources.PSObject.Properties | ForEach-Object {
+        $_.value | ForEach-Object {
+            $_.PSObject.Properties | ForEach-Object {
+                $_ | Select-Object @{ n='accesstoken'; e={ $RateLimitStatus.rate_limit_context.access_token }},
+                                   @{ n='resource'; e={ $_.name }},
+                                   @{ n='limit'; e={ $_.value.limit }},
+                                   @{ n='remaining'; e={ $_.value.remaining }},
+                                   @{ n='reset'; e={ $_.value.reset }}
             }
         }
     }
 
-    "/statuses/update" | Select-Object @{ n='accesstoken'; e={ $RateLimitStatus.rate_limit_context.access_token }}, @{ n='resource'; e={ $_ }}, @{ n='limit'; e={ 15 }}, @{ n='remaining'; e={ 15 }}, @{ n='reset'; e={ $Eposh.TotalSeconds }}
+    # No idea why, but '/statuses/update' is missing from the RateLimitStatus result.
+    "/statuses/update" | Select-Object @{ n='accesstoken'; e={ $RateLimitStatus.rate_limit_context.access_token }},
+                                       @{ n='resource'; e={ $_ }},
+                                       @{ n='limit'; e={ 15 }},
+                                       @{ n='remaining'; e={ 15 }},
+                                       @{ n='reset'; e={ $Eposh.TotalSeconds }}
 
 }
